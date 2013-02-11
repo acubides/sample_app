4a26dcf3ae5bdd6178e5eacfafb46a0a637fb303
Andrew Cubides <andrew.cubides@gmail.com>
2013-02-10 22:57:00 -0500

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :test do
 	gem 'guard-spork', '0.3.2'
 	gem 'spork', '0.9.0'
 	gem 'wdm', '0.0.3'
+  gem 'factory_girl_rails', '1.4.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do
+    name  "Andrew Cubides"
+    email "andrew.cubides@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
